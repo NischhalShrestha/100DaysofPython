59077653a307461dc68d9f6b49300ae39b7c19d2
NischhalShrestha <nischhalsth@gmail.com>
2025-01-22 08:53:48 +0545

    Caesar Cypher Complete

---
+++ Day 8/day_eight.py
@@ -0,0 +1,19 @@
+# def greet():
+#     print("Hello")
+#     print("bakery")
+#     print("Rainy")
+
+# greet()
+
+# #Function with an input
+# def name_greet(name):
+#     print(f"Hello {name}!")
+#     print(f"bakery {name}")
+
+# name_greet()
+
+#Function with multiple inputs
+def greet_name_location(name, location):
+    print(f"Hello  {name} from {location}")
+
+greet_name_location(name="Nischhal", location="Bhakapur")
